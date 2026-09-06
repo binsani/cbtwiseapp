da5e906a15d0aafc88de4b4497a77babf525b49a
SALIM ZAKARIYYA  SANI <s.sani3306@miva.edu.ng>
2026-09-06 23:00:20 +0100

    feat: auto-verify user and assign user role on registration

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -27,8 +27,12 @@ new #[Layout('layouts.guest')] class extends Component
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
+        $validated['email_verified_at'] = now();
 
-        event(new Registered($user = User::create($validated)));
+        $user = User::create($validated);
+        $user->assignRole('user');
+
+        event(new Registered($user));
 
         Auth::login($user);
 
