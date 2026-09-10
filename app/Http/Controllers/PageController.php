3dc18401255e3c6b68b11feca1ac88e6217d90fa
SALIM ZAKARIYYA  SANI <s.sani3306@miva.edu.ng>
2026-09-10 09:10:53 +0100

    fix(download): set direct fallback binary download URL for Windows desktop installer

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -70,7 +70,7 @@ class PageController extends Controller
             return response()->download($localPath, 'CBTwise-Setup.exe');
         }
 
-        return redirect('https://github.com/binsani/cbtwiseapp/releases');
+        return redirect('https://github.com/binsani/cbtwiseapp/releases/latest/download/CBTwise-Setup.exe');
     }
 
     public function refundPolicy()
